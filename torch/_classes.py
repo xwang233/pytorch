fbee1cd2baa150a51a988b0822e124980135efab
James Reed <jamesreed@fb.com>
2020-01-22 15:57:05 -0800

    [JIT] Add torch.classes.load_library
    ghstack-source-id: ea1377061f7bd3808c084729d77152b23e92cfb3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32508

--- torch/_classes.py
+++ torch/_classes.py
@@ -4,12 +4,16 @@ import torch._C
 class _Classes(types.ModuleType):
     def __init__(self):
         super(_Classes, self).__init__('torch.classes')
+        self.loaded_libraries = set()
 
     def __getattr__(self, attr):
         proxy = torch._C._get_custom_class_python_wrapper(attr)
         if proxy is None:
             raise RuntimeError('Class {} not registered!'.format(attr))
         return proxy
+
+    def load_library(self, path):
+        torch.ops.load_library(path)
 
 # The classes "namespace"
 classes = _Classes()

b58622daf3c890dab0c0799856e41af2ac3b5247
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2022-10-25 16:12:59 -0700

    add --run-in-parallel

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -1356,6 +1356,12 @@ def parse_args():
         help="Use a fresh triton cachedir when running each model, to force cold-start compile.",
     )
 
+    parser.add_argument(
+        "--run-in-parallel",
+        action="store_true",
+        help="Run different models on different CUDA devices in parallel to speedup benchmark process."
+    )
+
     group_fuser = parser.add_mutually_exclusive_group()
     # --nvfuser is now the default, keep the option to not break scripts
     group_fuser.add_argument("--nvfuser", action="store_true", help=argparse.SUPPRESS)
@@ -1791,19 +1797,54 @@ def main(runner, original_dir=None):
             os.unlink(output_filename)
         if original_dir:
             os.chdir(original_dir)
-        for name in runner.iter_model_names(args):
-            current_name = name
-            placeholder_batch_size = 0
-            try:
-                subprocess.check_call([sys.executable] + sys.argv + [f"--only={name}"])
-            except subprocess.SubprocessError:
-                print("ERROR")
-                for device in args.devices:
-                    output_csv(
-                        output_filename, [], [device, name, placeholder_batch_size, 0.0]
-                    )
+
+        if not args.run_in_parallel:
+            for name in runner.iter_model_names(args):
+                _model_run_helper(name, args, 1)
+        else:
+            assert torch.cuda.is_available()
+
+            import multiprocessing
+            p_nprocs = subprocess.run(['nproc'], stdout=subprocess.PIPE)
+            nprocs = int(p_nprocs.stdout)
+            ngpus = torch.cuda.device_count()
+            num_forks = max(1, min(ngpus, nprocs - 2))
+            print(f'run in parallel with {num_forks = }')
+
+            with multiprocessing.Pool(num_forks) as mp_pool:
+                mp_pool.starmap(_model_run_helper, ((_model, args, num_forks) for _model in runner.iter_model_names(args)))
+
         print_summary(output_filename)
 
+def _model_run_helper(name_, args, num_forks):
+    current_name = name_
+    placeholder_batch_size = 0
+
+    import sys
+    env = os.environ.copy()
+    stdout_stream = sys.stdout
+    stderr_stream = sys.stderr
+
+    run_in_parallel = args.run_in_parallel and num_forks > 1
+    if run_in_parallel:
+        import multiprocessing
+        gpu_index = multiprocessing.current_process()._identity[0] % num_forks
+        env['CUDA_VISIBLE_DEVICES'] = str(gpu_index)
+        stdout_stream = open(f'{name_}_{args.output[:-4]}.txt', 'w')
+        stderr_stream = subprocess.STDOUT
+        print(f'started model {name_} on {gpu_index = }')
+    try:
+        subprocess.check_call([sys.executable] + sys.argv + [f"--only={name_}"], env=env, stdout=stdout_stream, stderr=stderr_stream)
+    except subprocess.SubprocessError:
+        print("ERROR", f"on model {name_}" if run_in_parallel else "")
+        for device in args.devices:
+            output_csv(
+                output_filename, [], [device, name_, placeholder_batch_size, 0.0]
+            )
+    finally:
+        if run_in_parallel:
+            stdout_stream.close()
+            print(f'finished model {name_}')
 
 def log_operator_inputs(model, example_inputs, model_iter_fn, name, args):
     mode = "training" if args.training else "eval"

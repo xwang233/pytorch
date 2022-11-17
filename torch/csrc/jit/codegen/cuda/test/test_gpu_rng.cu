95c89c7dccf4ffab53a2ca31586ea43963e4d8b0
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2022-11-17 14:04:52 -0800

    refactor test

--- torch/csrc/jit/codegen/cuda/test/test_gpu_rng.cu
+++ torch/csrc/jit/codegen/cuda/test/test_gpu_rng.cu
@@ -25,21 +25,42 @@ namespace jit {
 
 using namespace torch::jit::fuser::cuda;
 
+enum RNGTest_t{
+  Uniform,
+  Normal,
+};
+
 namespace {
 
 template <typename T>
-__global__ void generate_uniform_kernel(
+__global__ void generate_random_numbers_kernel(
     T* output,
     int64_t size,
-    PhiloxCudaState philox_args) {
+    PhiloxCudaState philox_args,
+    RNGTest_t rng_test) {
   int64_t tid = blockIdx.x * blockDim.x + threadIdx.x;
 
   auto seeds = at::cuda::philox::unpack(philox_args);
   curandStatePhilox4_32_10_t state;
   curand_init(std::get<0>(seeds), tid, std::get<1>(seeds), &state);
 
+  double2 (*ref_rng_double)(curandStatePhilox4_32_10_t*);
+  float4 (*ref_rng_float)(curandStatePhilox4_32_10_t*);
+  switch (rng_test) {
+    case RNGTest_t::Uniform: {
+      ref_rng_double = curand_uniform2_double;
+      ref_rng_float = curand_uniform4;
+      break;
+    }
+    case RNGTest_t::Normal: {
+      ref_rng_double = curand_normal2_double;
+      ref_rng_float = curand_normal4;
+      break;
+    }
+  }
+
   if (std::is_same<T, double>::value) {
-    double2 result = curand_uniform2_double(&state);
+    double2 result = ref_rng_double(&state);
     if (tid * 2 < size) {
       output[tid * 2] = result.x;
     }
@@ -49,7 +70,7 @@ __global__ void generate_uniform_kernel(
   } else {
     auto is_float = std::is_same<T, float>::value;
     assert(is_float);
-    float4 result = curand_uniform4(&state);
+    float4 result = ref_rng_float(&state);
     if (tid * 4 < size) {
       output[tid * 4] = result.x;
     }
@@ -65,47 +86,7 @@ __global__ void generate_uniform_kernel(
   }
 }
 
-template <typename T>
-__global__ void generate_normal_kernel(
-    T* output,
-    int64_t size,
-    PhiloxCudaState philox_args) {
-  int64_t tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-  auto seeds = at::cuda::philox::unpack(philox_args);
-  curandStatePhilox4_32_10_t state;
-  curand_init(std::get<0>(seeds), tid, std::get<1>(seeds), &state);
-
-  if (std::is_same<T, double>::value) {
-    double2 result = curand_normal2_double(&state);
-    if (tid * 2 < size) {
-      output[tid * 2] = result.x;
-    }
-    if (tid * 2 + 1 < size) {
-      output[tid * 2 + 1] = result.y;
-    }
-  } else {
-    auto is_float = std::is_same<T, float>::value;
-    assert(is_float);
-    float4 result = curand_normal4(&state);
-    if (tid * 4 < size) {
-      output[tid * 4] = result.x;
-    }
-    if (tid * 4 + 1 < size) {
-      output[tid * 4 + 1] = result.y;
-    }
-    if (tid * 4 + 2 < size) {
-      output[tid * 4 + 2] = result.z;
-    }
-    if (tid * 4 + 3 < size) {
-      output[tid * 4 + 3] = result.w;
-    }
-  }
-}
-
-template <typename func_float_t, typename func_double_t>
-at::Tensor generate_random_numbers(int64_t size, at::ScalarType dtype,
-    func_float_t rng_float_kernel, func_double_t rng_double_kernel) {
+at::Tensor generate_random_numbers(int64_t size, at::ScalarType dtype, RNGTest_t rng_test) {
   auto options = at::TensorOptions().dtype(dtype).device(at::kCUDA, 0);
   auto result = at::empty({size}, options);
 
@@ -122,33 +103,33 @@ at::Tensor generate_random_numbers(int64_t size, at::ScalarType dtype,
     int64_t block = 128;
     int64_t block_elems = block * 4;
     int64_t grid = (size + block_elems - 1) / block_elems;
-    rng_float_kernel<<<
+    generate_random_numbers_kernel<<<
         grid,
         block,
         0,
         at::cuda::getCurrentCUDAStream()>>>(
-        result.data_ptr<float>(), size, rng_engine_inputs);
+        result.data_ptr<float>(), size, rng_engine_inputs, rng_test);
   } else {
     TORCH_CHECK(dtype == kDouble);
     int64_t block = 128;
     int64_t block_elems = block * 2;
     int64_t grid = (size + block_elems - 1) / block_elems;
-    rng_double_kernel<<<
+    generate_random_numbers_kernel<<<
         grid,
         block,
         0,
         at::cuda::getCurrentCUDAStream()>>>(
-        result.data_ptr<double>(), size, rng_engine_inputs);
+        result.data_ptr<double>(), size, rng_engine_inputs, rng_test);
   }
   return result;
 }
 
 at::Tensor generate_uniform(int64_t size, at::ScalarType dtype) {
-  return generate_random_numbers(size, dtype, generate_uniform_kernel<float>, generate_uniform_kernel<double>);
+  return generate_random_numbers(size, dtype, RNGTest_t::Uniform);
 }
 
 at::Tensor generate_normal(int64_t size, at::ScalarType dtype) {
-  return generate_random_numbers(size, dtype, generate_normal_kernel<float>, generate_normal_kernel<double>);
+  return generate_random_numbers(size, dtype, RNGTest_t::Normal);
 }
 
 } // namespace
